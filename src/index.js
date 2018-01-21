8457c48313d7c1cd5a49ca2f33ecbadb8a67ce76
Volodin Sergey <s.volodin@corp.mail.ru>
2018-01-21 15:59:55 +0300

    added store and reducer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import 'babel-polyfill'
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './containers/App';
+import configureStore from './store/configureStore';
+
+const store = configureStore();
 
 ReactDOM.render(
-	<App/>,
+	<Provider store={store}>
+		<App/>
+	</Provider>,
 	document.getElementById('root'),
 );
 
